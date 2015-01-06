c5d655a3c2e19a73b9024a65049fa4e6c8cde30e
Joseph Lundy <joseph.lundy456@gmail.com>
2015-01-05 21:37:16 -0500

    implements Piece class

--- app/src/main/java/com/example/joe/strategogame/gameRules/Piece.java
+++ app/src/main/java/com/example/joe/strategogame/gameRules/Piece.java
@@ -1,7 +1,47 @@
 package com.example.joe.strategogame.gameRules;
 
-/**
- * Created by joe on 2015-01-05.
- */
 public class Piece {
+    private int rank;
+    private int teamID;
+    private int pieceID;
+    protected int position;
+
+    public Piece(int rank, int team, int pieceID, int position)
+    {
+        this.rank = rank;
+        this.teamID = team;
+        this.pieceID = pieceID;
+        this.position = position;
+    }
+
+    public int getRank()
+    {
+        return rank;
+    }
+
+    public int getPosition()
+    {
+        return position;
+    }
+
+    public int getPieceID()
+    {
+        return pieceID;
+    }
+
+    public int getTeamID()
+    {
+        return teamID;
+    }
+
+    public void setPosition(int destination)
+    {
+        position = destination;
+    }
+
+    public boolean canDefeatPiece(int otherPieceRank)
+    {
+        return rank > otherPieceRank;
+    }
+
 }
