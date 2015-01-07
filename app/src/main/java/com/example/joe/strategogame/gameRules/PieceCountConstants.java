c2363b6b7c6e34b7bfc2b6fed21ffb2623408c0c
Joseph Lundy <joseph.lundy456@gmail.com>
2015-01-06 21:54:25 -0500

    adds piece counts

---
+++ app/src/main/java/com/example/joe/strategogame/gameRules/PieceCountConstants.java
@@ -0,0 +1,15 @@
+package com.example.joe.strategogame.gameRules;
+
+public class PieceCountConstants {
+    public static int FLAG = 1;
+    public static int BOMB = 6;
+    public static int SCOUT = 8;
+    public static int MINER = 5;
+    public static int SERGEANT = 4;
+    public static int LIEUTENANT = 4;
+    public static int CAPTAIN = 4;
+    public static int MAJOR = 3;
+    public static int COLONEL = 2;
+    public static int GENERAL = 1;
+    public static int MARSHALL = 1;
+}
